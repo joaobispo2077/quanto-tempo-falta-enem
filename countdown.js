a235373a307516413bd6068ae3170740b612556d
João Bispo <joaobispo2077@gmail.com>
2021-06-26 18:13:10 -0300

    feat: update ENEM date

--- countdown.js
+++ countdown.js
@@ -1,39 +1,39 @@
 function countdown() {
 
-    var now = new Date();
+	var now = new Date();
 
-    var eventDate = new Date(2021, 0, 17, 13, 0, 0, 0);
-    console.log(`A data do enem é ${eventDate}`);
+	var eventDate = new Date(2021, 10, 21, 13, 0, 0, 0);
+	console.log(`A data do enem é ${eventDate}`);
 
-    var currentTime = now.getTime();
-    var eventTime = eventDate.getTime();
+	var currentTime = now.getTime();
+	var eventTime = eventDate.getTime();
 
-    var remTime = eventTime - currentTime;
+	var remainingTime = eventTime - currentTime;
 
-    var s = Math.floor(remTime / 1000);
-    var m = Math.floor(s / 60);
-    var h = Math.floor(m / 60);
-    var d = Math.floor(h / 24);
+	var s = Math.floor(remainingTime / 1000);
+	var m = Math.floor(s / 60);
+	var h = Math.floor(m / 60);
+	var d = Math.floor(h / 24);
 
-    h %= 24;
-    m %= 60;
-    s %= 60;
+	h %= 24;
+	m %= 60;
+	s %= 60;
 
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
+	h = (h < 10) ? "0" + h : h;
+	m = (m < 10) ? "0" + m : m;
+	s = (s < 10) ? "0" + s : s;
 
-    document.getElementById("days").textContent = d;
-    document.getElementById("days").innerText = d;
+	document.getElementById("days").textContent = d;
+	document.getElementById("days").innerText = d;
 
-    document.getElementById("hours").textContent = h;
-    document.getElementById("minutes").textContent = m;
-    document.getElementById("seconds").textContent = s;
+	document.getElementById("hours").textContent = h;
+	document.getElementById("minutes").textContent = m;
+	document.getElementById("seconds").textContent = s;
 
-    console.log(d, h, m, s);
+	console.log(d, h, m, s);
 
 
-    setTimeout(countdown, 1000);
+	setTimeout(countdown, 1000);
 }
 
 countdown();
